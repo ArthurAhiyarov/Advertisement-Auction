a3303fdeca99c9813b238af2683a4b996ddfe786
Arthur <arakhiyarov@gmail.com>
2022-08-11 19:42:00 +0500

    deploy changes

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -13,12 +13,5 @@ def deploy_adv_auction(front_end_update=False):
         update_front_end()
     return adv_auction
 
-def update_front_end():
-    with open('brownie-config.yaml', 'r') as brownie_config:
-        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
-        with open('./front_end/src/brownie-config.json', 'w') as brownie_config_json:
-            json.dump(config_dict, brownie_config_json)
-    print('Front end updated!')
-
 def main():
     deploy_adv_auction(front_end_update=True)
